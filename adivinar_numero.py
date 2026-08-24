ffe58ea3f34b6056ab25365ed496b0256fd63023
Gabriel <gabo.gh12398@gmail.com>
2026-08-24 11:26:03 -0300

    Proyecto adivinar número

---
+++ adivinar_numero.py
@@ -0,0 +1,76 @@
+import random
+
+RESET = "\033[0m"
+ROJO = "\033[91m"
+VERDE = "\033[92m"
+AMARILLO = "\033[93m"
+AZUL = "\033[94m"
+CIAN = "\033[96m"
+
+print(f"{AZUL}================================{RESET}")
+print(F"""{AZUL}
+       ADIVINA EL NÚMERO
+       {RESET}""")
+print(F"{AZUL}================================{RESET}")
+
+nombre = input("¡Hola! ¿Cómo te llamas?\n")
+
+print(f"Hola {CIAN}{nombre}{RESET} 👋")
+
+max = random.randint(10, 100)
+
+print(f"Estoy pensando en un número entre el 1 y el {max}")
+
+adivina = random.randint(1, max)
+
+intentos = 0
+nums_string = ""
+
+nums = []
+
+while True:
+    num = int(input("¿Cuál es el número?\n"))
+
+    if intentos == 5:
+        tipo = random.randint(1, 2)
+        pista = -1
+
+        if tipo == 1:
+            pista = random.randint(1, adivina-1)
+            while pista in nums:
+                pista = random.randint(1, adivina-1)
+            print(f"{AMARILLO}PISTA: el número es mayor que {pista}{RESET}")
+        else:
+            pista = random.randint(adivina+1, max)
+            while pista in nums:
+                pista = random.randint(adivina+1, max)
+            print(f"{AMARILLO}PISTA: el número es menor que {pista}{RESET}")
+        nums.append(pista)
+        nums.sort()
+        for i in nums:
+            nums_string = ", ".join(str(i) for i in nums)
+    
+    if num == adivina:
+        print(f"{VERDE}¡Correcto! {RESET}🎉")
+        print(F"{VERDE}Ganaste.{RESET}")
+        print(f"{VERDE}Lo lograste en {intentos} intentos{RESET}")
+        break
+    elif num in nums:
+        print(F"{ROJO}Ya dijiste este número{RESET}")
+        continue
+    elif num > adivina:
+        print(F"{ROJO}Incorrecto, el número es menor{RESET}")
+    elif num < adivina:
+        print(F"{ROJO}Incorrecto, el número es mayor{RESET}")
+    intentos += 1
+
+    nums.append(num)
+    nums.sort()
+    nums_string = ""
+    if len(nums) == 1:
+        nums_string = nums[0]
+    else:
+        for i in nums:
+            nums_string = ", ".join(str(i) for i in nums)
+
+    print(f"{CIAN}Números descartados: {nums_string}{RESET}")
